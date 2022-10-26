2dc450831bbaf1787fd9a27cf6dbdfd03557c21b
daniel.bannert <daniel.bannert@limango.com>
2022-10-26 10:48:20 +0200

    refactor(readdir): moved fixtures out of tests folder

--- packages/readdir/__tests__/walk.test.ts
+++ packages/readdir/__tests__/walk.test.ts
@@ -1,6 +1,7 @@
 import {
     describe, expect, it,
 } from "vitest";
+import path from "path";
 
 import { walk } from "../src";
 
@@ -9,17 +10,17 @@ describe("walk", () => {
         const files: string[] = [];
 
         // eslint-disable-next-line no-restricted-syntax, unicorn/prefer-module
-        for await (const index of walk(`${__dirname}/fixtures`, {})) {
+        for await (const index of walk(`${__dirname}/../__fixtures__`, {})) {
             files.push(index.path);
         }
 
         expect(files).toEqual([
             // eslint-disable-next-line unicorn/prefer-module
-            `${__dirname}/fixtures`,
+            path.join(__dirname, '..', '/__fixtures__'),
             // eslint-disable-next-line unicorn/prefer-module
-            `${__dirname}/fixtures/test.js`,
+            path.join(__dirname, '..', '/__fixtures__', '/test.js'),
             // eslint-disable-next-line unicorn/prefer-module
-            `${__dirname}/fixtures/test2.ts`,
+            path.join(__dirname, '..', '/__fixtures__', '/test2.ts'),
         ]);
     });
 });
