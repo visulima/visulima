be3a7d0c9e7bcb23a31e8d2d57e39112c218509d
daniel.bannert <daniel.bannert@limango.com>
2022-11-15 14:25:42 +0100

    feat(crud): removed default allowedMediaTypes

--- examples/api-platform/pages/api/swagger.ts
+++ examples/api-platform/pages/api/swagger.ts
@@ -1,3 +1,13 @@
 import { swaggerApiRoute } from "@visulima/api-platform/next";
 
-export default swaggerApiRoute();
+export default swaggerApiRoute({
+    allowedMediaTypes: {
+        "application/json": true,
+        "application/vnd.api+json": true,
+        "application/x-yaml": true,
+        "application/xml": true,
+        "text/csv": true,
+        "text/html": true,
+        "text/xml": true,
+    },
+});
--- examples/crud/pages/api/swagger.ts
+++ examples/crud/pages/api/swagger.ts
@@ -3,6 +3,15 @@ import { swaggerApiRoute } from "@visulima/api-platform/next";
 import { prisma } from "../../lib/prisma-client";
 
 export default swaggerApiRoute({
+    allowedMediaTypes: {
+        "application/json": true,
+        "application/vnd.api+json": true,
+        "application/x-yaml": true,
+        "application/xml": true,
+        "text/csv": true,
+        "text/html": true,
+        "text/xml": true,
+    },
     crud: {
         prismaClient: prisma,
     },
