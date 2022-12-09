e2f19782b63bc0a1c70bd19be702eff6b142c585
daniel.bannert <daniel.bannert@limango.com>
2022-12-09 19:43:10 +0100

    fix(nextra-theme-docs): use full with if next or prev is missing

--- packages/nextra-theme-docs/src/components/nav-links.tsx
+++ packages/nextra-theme-docs/src/components/nav-links.tsx
@@ -16,7 +16,7 @@ interface NavLinkProperties {
 const classes = {
     link: cn(
         // eslint-disable-next-line max-len
-        "flex max-w-[50%] items-center gap-1 py-4 text-base font-medium text-gray-600 transition-colors [word-break:break-word] hover:text-primary-600 dark:text-gray-300 md:text-lg",
+        "flex items-center gap-1 py-4 text-base font-medium text-gray-600 transition-colors [word-break:break-word] hover:text-primary-600 dark:text-gray-300 md:text-lg",
     ),
     icon: cn("inline h-5 shrink-0"),
 };
@@ -45,6 +45,7 @@ const NavLinks: FC<NavLinkProperties> = ({ flatDirectories, currentIndex }) => {
                     title={previous.title}
                     className={cn(
                         classes.link,
+                        next ? "max-w-[50%]": "max-w-full",
                         "ltr:pr-4 rtl:pl-4",
                         "flex-grow mr-4 border border-solid dark:border-primary-100/10 transition-all hover:-translate-y-1",
                     )}
@@ -62,6 +63,7 @@ const NavLinks: FC<NavLinkProperties> = ({ flatDirectories, currentIndex }) => {
                     title={next.title}
                     className={cn(
                         classes.link,
+                        previous ? "max-w-[50%]": "max-w-full",
                         "ltr:ml-auto ltr:pl-4 ltr:text-right rtl:mr-auto rtl:pr-4 rtl:text-left",
                         "flex-grow ml-4 border border-solid dark:border-primary-100/10 transition-all hover:-translate-y-1",
                     )}
