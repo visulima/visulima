71b99d3182638af4e040fbecce087534373d8d4a
daniel.bannert <daniel.bannert@limango.com>
2022-12-10 02:05:04 +0100

    chore(nextra-theme-docs): fixed react in vitest

--- packages/nextra-theme-docs/vitest.config.ts
+++ packages/nextra-theme-docs/vitest.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // plugins: [react()],
+    plugins: [react()],
     // test: {
     //   environment: 'jsdom',
     // },
