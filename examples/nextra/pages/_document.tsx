b1a5c8236f56bb52caaca0b36716142bcc34ccf3
daniel.bannert <daniel.bannert@limango.com>
2022-12-10 13:59:31 +0100

    feat(nextra-theme-docs): added document page to example

---
+++ examples/nextra/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { SkipNavLink } from "@visulima/nextra-theme-docs";
+import Document, {
+ Head, Html, Main, NextScript,
+} from "next/document";
+import React from "react";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body className="relative">
+                    <SkipNavLink />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
