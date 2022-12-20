8d36b3a1aa127e64f3b116a6deb9b2cc51408f2f
daniel.bannert <daniel.bannert@limango.com>
2022-12-20 13:17:05 +0100

    fix(uploads): update lock file

--- packages/uploads/__tests__/storage/aws/s3-file.test.ts
+++ packages/uploads/__tests__/storage/aws/s3-file.test.ts
@@ -1,11 +1,12 @@
 import {
     describe, expect, it,
 } from "vitest";
+
 import S3File from "../../../src/storage/aws/s3-file";
 
 describe("S3File", () => {
     it("should have extra params", () => {
-        const file = new S3File({ metadata: { foo: "bar" },  });
+        const file = new S3File({ metadata: { foo: "bar" } });
 
         file.uri = "uri";
         file.Parts = [];
@@ -14,5 +15,5 @@ describe("S3File", () => {
         file.partSize = 1;
 
         expect(file).toMatchSnapshot();
-    })
+    });
 });
