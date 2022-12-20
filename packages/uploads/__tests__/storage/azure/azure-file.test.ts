5bb1a3e9fa39643da3741abfbf042a474ceae73f
daniel.bannert <daniel.bannert@limango.com>
2022-12-20 13:17:05 +0100

    fix(uploads): update lock file

--- packages/uploads/__tests__/storage/azure/azure-file.test.ts
+++ packages/uploads/__tests__/storage/azure/azure-file.test.ts
@@ -1,15 +1,16 @@
 import {
     describe, expect, it,
 } from "vitest";
+
 import AzureFile from "../../../src/storage/azure/azure-file";
 
 describe("AzureFile", () => {
     it("should have extra params", () => {
-        const file = new AzureFile({ metadata: { foo: "bar" },  });
+        const file = new AzureFile({ metadata: { foo: "bar" } });
 
         file.requestId = "requestId";
         file.uri = "uri";
 
         expect(file).toMatchSnapshot();
-    })
+    });
 });
