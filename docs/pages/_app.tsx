0e72ae326792c1e8f297b1a3c81164728b9871c4
prisis <d.bannert@anolilab.de>
2023-05-16 01:00:45 +0200

    fix: try to fix nextra

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -5,15 +5,11 @@ import type { AppProps } from "next/app";
 import type { FC, ReactElement } from "react";
 import React from "react";
 
-import reportAccessibility from "../src/utils/report-accessibility";
-
 const MyApp: FC<AppProps & { Component: AppProps["Component"] & { getLayout?: (component: ReactElement) => ReactElement } }> = ({ Component, pageProps }) => {
     const getLayout = Component.getLayout ?? ((page) => page);
 
     // eslint-disable-next-line react/jsx-props-no-spreading
     return getLayout(<Component {...pageProps} />);
 };
-
-reportAccessibility(React);
 
 export default MyApp;
