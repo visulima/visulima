edc0e6e5cfef0b93a4280f67cd2dec37581e0dc6
prisis <d.bannert@anolilab.de>
2023-05-29 20:10:12 +0200

    fix: added read-pkg-up to check if the first found package.json has the type key to create the correct config file export

--- packages/jsdoc-open-api/__tests__/cli/command/init-command.test.ts
+++ packages/jsdoc-open-api/__tests__/cli/command/init-command.test.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-namespace
 import * as fs from "node:fs";
+import { join } from "node:path";
 import {
     describe, expect, it, vi,
 } from "vitest";
@@ -10,11 +11,12 @@ vi.mock("node:fs", () => {
     return {
         existsSync: vi.fn(),
         writeFileSync: vi.fn(),
+        realpathSync: vi.fn(),
     };
 });
 
 describe("init command", () => {
-    it("throws an error when the config file already exists", () => {
+    it("should throw an error when the config file already exists", () => {
         vi.spyOn(fs, "existsSync").mockReturnValue(true);
         vi.spyOn(console, "log");
 
@@ -22,7 +24,7 @@ describe("init command", () => {
         expect(() => initCommand("config.js")).toThrowError("Config file already exists");
     });
 
-    it("creates a new config file with the correct contents", () => {
+    it("should create a new config file with the correct module.exports content", () => {
     // Create a mock for the existsSync function to return false
         vi.spyOn(fs, "existsSync").mockReturnValue(false);
         vi.spyOn(console, "log");
@@ -72,7 +74,65 @@ describe("init command", () => {
         );
     });
 
-    it("logs a message when the config file is created", () => {
+    it("should create a new config file with the correct export default content", () => {
+        // Create a mock for the existsSync function to return false
+        vi.spyOn(fs, "existsSync").mockReturnValue(false);
+        vi.spyOn(console, "log");
+
+        // eslint-disable-next-line unicorn/prefer-module
+        const fixturePath = join(__dirname, "../../../", "__fixtures__");
+
+        vi.spyOn(fs, "realpathSync").mockReturnValue(fixturePath);
+
+        const consoleInfoMock = vi.spyOn(console, "info");
+        const writeFileSyncMock = vi.spyOn(fs, "writeFileSync");
+
+        initCommand("config.js");
+
+        expect(consoleInfoMock).toHaveBeenCalledWith(`Found package.json at "${fixturePath}/package.json"`);
+        expect(consoleInfoMock).toHaveBeenCalledWith("Found package.json with type: module, using ES6 as export for the config file");
+        // Verify that the writeFileSync function was called with the correct arguments
+        expect(writeFileSyncMock).toHaveBeenCalledWith(
+            "config.js",
+            `export default {
+  exclude: [
+    'coverage/**',
+    '.github/**',
+    'packages/*/test{,s}/**',
+    '**/*.d.ts',
+    'test{,s}/**',
+    'test{,-*}.{js,cjs,mjs,ts,tsx,jsx,yaml,yml}',
+    '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx,yaml,yml}',
+    '**/__tests__/**',
+    '**/{ava,babel,nyc}.config.{js,cjs,mjs}',
+    '**/jest.config.{js,cjs,mjs,ts}',
+    '**/{karma,rollup,webpack}.config.js',
+    '**/.{eslint,mocha}rc.{js,cjs}',
+    '**/.{travis,yarnrc}.yml',
+    '**/{docker-compose,docker}.yml',
+    '**/.yamllint.{yaml,yml}',
+    '**/node_modules/**',
+    '**/pnpm-lock.yaml',
+    '**/pnpm-workspace.yaml',
+    '**/{package,package-lock}.json',
+    '**/yarn.lock',
+    '**/package.json5',
+    '**/.next/**',
+  ],
+  followSymlinks: false,
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API',
+      version: '1.0.0',
+    },
+  },
+};
+`,
+        );
+    });
+
+    it("should log a message when the config file is created", () => {
         vi.spyOn(fs, "existsSync").mockReturnValue(false);
 
         const consoleLogMock = vi.spyOn(console, "log");
--- packages/jsdoc-open-api/src/cli/command/init-command.ts
+++ packages/jsdoc-open-api/src/cli/command/init-command.ts
@@ -1,13 +1,39 @@
-import { existsSync, writeFileSync } from "node:fs";
+import { existsSync, realpathSync, writeFileSync } from "node:fs";
+import readPkgUp from "read-pkg-up";
 
-const initCommand = (configName: string): void => {
+const initCommand = (configName: string, packageJsonPath = process.cwd()): void => {
     if (existsSync(configName)) {
         throw new Error("Config file already exists");
     }
 
+    const foundPackageJson = readPkgUp.sync({
+        // eslint-disable-next-line no-undef
+        cwd: realpathSync(packageJsonPath),
+    });
+
+    let exportTemplate = "module.exports =";
+
+    if (foundPackageJson) {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        const { packageJson: package_, path: packagePath } = foundPackageJson;
+
+        // eslint-disable-next-line no-console
+        console.info(`Found package.json at "${packagePath}"`);
+
+        if (package_["type"] === "module") {
+            // eslint-disable-next-line no-console
+            console.info("Found package.json with type: module, using ES6 as export for the config file");
+
+            exportTemplate = "export default";
+        }
+    } else {
+        // eslint-disable-next-line no-console
+        console.info("No package.json found");
+    }
+
     writeFileSync(
         configName,
-        `module.exports = {
+        `${exportTemplate} {
   exclude: [
     'coverage/**',
     '.github/**',
