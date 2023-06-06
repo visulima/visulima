ae9ad33e4ec3b5fa2dd8c0dbd291643a490885c3
prisis <d.bannert@anolilab.de>
2023-06-06 15:27:23 +0200

    style: cs fixes

--- packages/jsdoc-open-api/src/validate.ts
+++ packages/jsdoc-open-api/src/validate.ts
@@ -1,5 +1,5 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
-import OpenAPI from "openapi-types";
+import type OpenAPI from "openapi-types";
 
 const validate = async (spec: Record<string, unknown>): Promise<void> => {
     await SwaggerParser.validate(spec as OpenAPI.OpenAPI.Document);
