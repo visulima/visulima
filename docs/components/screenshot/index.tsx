609c378905bdcae3dc1b48dfef4053f41b33a822
prisis <d.bannert@anolilab.de>
2023-06-18 21:22:50 +0200

    chore: added zoom to screenshot, updated deps

--- docs/components/screenshot/index.tsx
+++ docs/components/screenshot/index.tsx
@@ -1,10 +1,13 @@
 import cn from "clsx";
 import Image from "next/image";
 import type { FC } from "react";
+import { Zoom } from "@visulima/nextra-theme-docs";
 
 const Screenshot: FC<{ alt: string; full: boolean, src: string; }> = ({ alt, full, src }) => (
     <div className={cn("-mb-4 mt-6 flex justify-center overflow-hidden rounded-xl border dark:border-zinc-800", full ? "bg-white" : "bg-zinc-100")}>
-        <Image alt={alt} className={cn("w-auto select-none bg-white", full ? "" : "ring-1 ring-gray-200")} src={src} />
+        <Zoom>
+            <Image src={src} alt={alt} className={cn("w-auto select-none bg-white", full ? "" : "ring-1 ring-gray-200")} />
+        </Zoom>
     </div>
 );
 
