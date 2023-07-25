359856f499fcf839c713c71d66f21e73cbfb4fef
prisis <d.bannert@anolilab.de>
2023-07-26 00:31:43 +0200

    fix: Update src codes, packages configs and formatter configs
    This commit includes multiple updates to Prisma DMMF transformer. Changes in src
    codes mainly focus on the modification of getPropertyDefinition,
    getJSONSchemaScalar and getJSONSchemaType, etc. to improve the performance.
    
    Several package related configuration files are also modified, such as
    .secretlintignore, .eslintignore and .prettierignore etc., aiming to ignore
    unnecessary files and folders.
    
    In formatter related configurations(.eslintrc.js and .prettierrc.js), changes are made
    to improve code style checking and formatting.

--- packages/prisma-dmmf-transformer/src/types.d.ts
+++ packages/prisma-dmmf-transformer/src/types.d.ts
@@ -4,9 +4,9 @@ import type { JSONSchema7Definition } from "json-schema";
 export type PrismaPrimitive = "BigInt" | "Boolean" | "Bytes" | "DateTime" | "Decimal" | "Float" | "Int" | "Json" | "String";
 
 export interface PropertyMetaData {
-    required: boolean;
     hasDefaultValue: boolean;
     isScalar: boolean;
+    required: boolean;
 }
 
 export interface ModelMetaData {
@@ -17,8 +17,8 @@ export type DefinitionMap = [name: string, definition: JSONSchema7Definition];
 export type PropertyMap = [...DefinitionMap, PropertyMetaData];
 
 export interface TransformOptions {
-    keepRelationScalarFields?: "false" | "true";
-    schemaId?: string;
     includeRequiredFields?: "false" | "true";
+    keepRelationScalarFields?: "false" | "true";
     persistOriginalType?: "false" | "true";
+    schemaId?: string;
 }
