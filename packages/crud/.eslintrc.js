599967b91f96b59c4ac51c53af970e64078595bc
prisis <d.bannert@anolilab.de>
2023-09-04 09:40:57 +0200

    fix: fixed test for windows
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/crud/.eslintrc.js
+++ packages/crud/.eslintrc.js
@@ -57,6 +57,13 @@ module.exports = {
                 "no-secrets/no-secrets": "off",
             },
         },
+        {
+            files: ["__tests__/integration/adapter/prisma/index.test.ts"],
+            rules: {
+                "vitest/require-hook": "off",
+                "vitest/require-top-level-describe": "off",
+            },
+        },
     ],
     parserOptions: {
         ecmaVersion: 2021,
