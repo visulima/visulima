d7c5b811be22434390170f2e1731d8d055660d5a
prisis <d.bannert@anolilab.de>
2023-09-04 10:48:14 +0200

    chore: fixed prisma install
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/crud/__tests__/integration/adapter/prisma/client.ts
+++ packages/crud/__tests__/integration/adapter/prisma/client.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+// eslint-disable-next-line n/no-unpublished-import
+import { PrismaClient } from "./prisma-client";
 
 const prisma = new PrismaClient();
 
