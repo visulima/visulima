d0495c3ec15dfc281584dae5d6dec72587f7fd08
prisis <d.bannert@anolilab.de>
2023-09-06 12:53:29 +0200

    fix: added swagger 4 and 5 example
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/crud/src/adapter/prisma/utils/models-to-route-names.ts
+++ packages/crud/src/adapter/prisma/utils/models-to-route-names.ts
@@ -1,9 +1,10 @@
 type RouteMap<M extends string> = { [key in M]?: string };
 
-const modelsToRouteNames = <M extends string = string>(mappingsMap: Record<M, Record<"name" | "plural", string>>, models: M[]): RouteMap<M> => {
+const modelsToRouteNames = <M extends string = string>(mappingsMap: Record<string, object>, models: M[]): RouteMap<M> => {
     const routesMap: RouteMap<M> = {};
 
     models.forEach((model) => {
+        // @ts-expect-error - This is a private property, but it's the only way to get the plural name
         routesMap[model as M] = mappingsMap[model as M].plural;
     });
 
