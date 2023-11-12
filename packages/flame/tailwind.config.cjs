7d7725bbe85e884f768302abeca87a8b3650b34d
prisis <d.bannert@anolilab.de>
2023-11-13 00:05:04 +0100

    feat: more work on flame
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/flame/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/template/**/*.ts"],
+    darkMode: ["class", 'html[class~="dark"]'],
+    theme: {
+        screens: {
+            "2xl": "1536px",
+            lg: "1024px",
+            md: "768px",
+            sm: "640px",
+            xl: "1280px",
+        },
+    },
+};
