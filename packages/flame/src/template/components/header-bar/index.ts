75054d94880f9c453ebdecb99b88d7cb283d889e
prisis <d.bannert@anolilab.de>
2023-12-03 01:25:27 +0100

    feat: first working version of flame
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/flame/src/template/components/header-bar/index.ts
@@ -0,0 +1,21 @@
+import themeToggle from "./theme-toggle";
+import type { Editor, Theme } from "../../../types";
+import editorSelector from "./editor-selector";
+
+const headerBar = (options: Partial<{ editor: Editor; theme: Theme }>): {
+    html: string;
+    script: string;
+} => {
+    const toggle = themeToggle(options?.theme);
+
+    return {
+        html: `<div class="my-4 w-full flex">
+    <div class="flex-grow"></div>
+    ${editorSelector(options?.editor)}
+    ${toggle.html}
+</div>`,
+        script: toggle.script,
+    };
+};
+
+export default headerBar;
--- packages/flame/src/template/index.ts
+++ packages/flame/src/template/index.ts
@@ -1,16 +1,71 @@
-import { parseStacktrace } from "@visulima/error";
+import { VisulimaError } from "@visulima/error/error";
 
-import titleBar from "./components/title-bar";
+import process from "../util/process";
+import runtimeName from "../util/runtimes";
+import errorCard from "./components/error-card";
+import rawStackTrace from "./components/raw-stack-trace";
 import inlineCss from "./index.css";
+import prelineJs from "inline:../../node_modules/preline/dist/preline.js";
+import clipboardJs from "inline:../../node_modules/clipboard/dist/clipboard.min.js";
 import layout from "./layout";
+import stackTraceViewer from "./components/stack-trace-viewer";
+import headerBar from "./components/header-bar";
+import type { Editor, Hint, Theme } from "../types";
 
-const index = (error: Error) => {
-    const { message, name } = error;
-    const traces = parseStacktrace(error);
-console.log(traces)
-    const html = titleBar(name, message, "");
+type HintError = Error & { hint: string | string[] | Hint | undefined };
 
-    return layout("Error", "Error", inlineCss, html);
+const getHint = (error: HintError): Hint | undefined => {
+    if (typeof error.hint === "undefined") {
+        return undefined;
+    }
+
+    if (typeof error.hint === "string") {
+        return { body: error.hint };
+    }
+
+    if (typeof error.hint === "object") {
+        return error.hint as Hint;
+    }
+
+    if (Array.isArray(error.hint)) {
+        return { body: (error.hint as string[]).join("\n") };
+    }
+
+    return undefined;
+};
+
+const index = async (error: Error | VisulimaError, options: Partial<{ editor: Editor; theme: Theme }> = {}): Promise<string> => {
+    const { message, name, stack } = error;
+
+    let html = "";
+    let script = "";
+
+    script += clipboardJs;
+    script += prelineJs;
+
+    const { html: headerBarHtml, script: headerBarScript } = headerBar(options);
+
+    html += headerBarHtml;
+    script += headerBarScript;
+
+    html += errorCard({
+        hint: getHint(error as HintError),
+        message,
+        runtimeName,
+        title: name,
+        version: process.version,
+    });
+    html += await stackTraceViewer(error);
+    html += rawStackTrace(stack);
+
+    return layout({
+        title: "Error",
+        description: "Error",
+        css: inlineCss.trim(),
+        script: script.trim(),
+        content: html.trim(),
+        error,
+    });
 };
 
 export default index;
