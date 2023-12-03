eb6771be172b6ce16cc19265a8a15088b75ccefb
prisis <d.bannert@anolilab.de>
2023-12-03 01:25:27 +0100

    feat: first working version of flame
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/flame/src/template/components/raw-stack-trace.ts
@@ -0,0 +1,12 @@
+const rawStackTrace = (stack?: string): string => {
+    return `
+    <section class="container bg-white dark:shadow-none dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 my-6">
+        <main id="raw-stack-trace">
+            <div class="px-4 py-5 sm:px-6">
+                ${stack}
+            </div>
+        </main>
+    </section>`;
+};
+
+export default rawStackTrace;
--- packages/flame/src/template/layout.ts
+++ packages/flame/src/template/layout.ts
@@ -1,4 +1,20 @@
-const layout = (title: string, description: string, css: string, content: string): string => `
+const layout = ({
+    title,
+    description,
+    content,
+    css,
+    script,
+    error,
+}: {
+    title: string;
+    description: string;
+    css: string;
+    script: string;
+    content: string;
+    error: Error,
+}): string => `<!--
+${error?.stack ? error.stack?.replace(/\n/g, "\n\t") : error.toString()}
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +22,67 @@ const layout = (title: string, description: string, css: string, content: string
     <title>${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="${description}">
-    ${css ? `<style>${css}</style>` : ""}
+    <style>${css}</style>
+    <script>${script}</script>
+    <script>
+      (function() {
+        window.addEventListener('load', () => {
+          const $clipboards = document.querySelectorAll('.js-clipboard');
+          $clipboards.forEach((el) => {
+            const isToggleTooltip = HSStaticMethods.getClassProperty(el, '--is-toggle-tooltip') === 'false' ? false : true;
+            const clipboard = new ClipboardJS(el, {
+              text: (trigger) => {
+                const clipboardText = trigger.dataset.clipboardText;
+
+                if (clipboardText) return clipboardText;
+
+                const clipboardTarget = trigger.dataset.clipboardTarget;
+                const $element = document.querySelector(clipboardTarget);
+
+                if (
+                  $element.tagName === 'SELECT'
+                  || $element.tagName === 'INPUT'
+                  || $element.tagName === 'TEXTAREA'
+                ) return $element.value
+                else return $element.textContent;
+              }
+            });
+            clipboard.on('success', () => {
+              const $default = el.querySelector('.js-clipboard-default');
+              const $success = el.querySelector('.js-clipboard-success');
+              const $successText = el.querySelector('.js-clipboard-success-text');
+              const successText = el.dataset.clipboardSuccessText || '';
+              const tooltip = el.closest('.hs-tooltip');
+              const $tooltip = HSTooltip.getInstance(tooltip, true);
+              let oldSuccessText;
+
+              if ($successText) {
+                oldSuccessText = $successText.textContent
+                $successText.textContent = successText
+              }
+              if ($default && $success) {
+                $default.style.display = 'none'
+                $success.style.display = 'block'
+              }
+              if (tooltip && isToggleTooltip) HSTooltip.show(tooltip);
+              if (tooltip && !isToggleTooltip) $tooltip.element.popperInstance.update();
+
+              setTimeout(function () {
+                if ($successText && oldSuccessText) $successText.textContent = oldSuccessText;
+                if (tooltip && isToggleTooltip) HSTooltip.hide(tooltip);
+                if (tooltip && !isToggleTooltip) $tooltip.element.popperInstance.update();
+                if ($default && $success) {
+                  $success.style.display = '';
+                  $default.style.display = '';
+                }
+              }, 800);
+            });
+          });
+        })
+      })()
+    </script>
 </head>
-<body>
+<body class="bg-gray-100">
     <div class="container mx-auto">
         <div class="flex flex-wrap">
             ${content}
@@ -16,6 +90,9 @@ const layout = (title: string, description: string, css: string, content: string
     </div>
 </body>
 </html>
+<!--
+${error?.stack ? error.stack?.replace(/\n/g, "\n\t") : error.toString()}
+-->
 `;
 
 export default layout;
