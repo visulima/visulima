7e9e3481f9049712c94362540425ac53f7428db7
prisis <d.bannert@anolilab.de>
2023-12-05 00:30:05 +0100

    feat: more work on flame
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/flame/src/template/components/header-bar/index.ts
+++ packages/flame/src/template/components/header-bar/index.ts
@@ -1,17 +1,17 @@
-import themeToggle from "./theme-toggle";
 import type { Editor, Theme } from "../../../types";
 import editorSelector from "./editor-selector";
+import themeToggle from "./theme-toggle";
 
 const headerBar = (options: Partial<{ editor: Editor; theme: Theme }>): {
     html: string;
     script: string;
 } => {
-    const toggle = themeToggle(options?.theme);
+    const toggle = themeToggle(options.theme);
 
     return {
         html: `<div class="my-4 w-full flex">
     <div class="flex-grow"></div>
-    ${editorSelector(options?.editor)}
+    ${editorSelector(options.editor)}
     ${toggle.html}
 </div>`,
         script: toggle.script,
