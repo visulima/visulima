b6f49175e6b9d4215e9658204a26fe18e867b575
prisis <d.bannert@anolilab.de>
2024-01-30 21:43:05 +0100

    chore: update dev deps
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,7 +6,7 @@ import type { Preview, ReactRenderer, StoryContext } from "@storybook/react";
 
 const preview: Preview = {
     decorators: [
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-unsafe-return
+        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-unsafe-return
         (storyFunction, context: StoryContext) => withConsole({})(storyFunction)(context),
         withThemeByClassName<ReactRenderer>({
             defaultTheme: "light",
