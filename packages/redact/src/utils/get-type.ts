4578fc6dfe0856a1e079d8a798b5b17c3b6f79f5
prisis <d.bannert@anolilab.de>
2024-02-09 22:50:30 +0100

    feat: improved deep-clone
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/redact/src/utils/get-type.ts
+++ packages/redact/src/utils/get-type.ts
@@ -1,2 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
-export const getType = (thing: any): string => Object.prototype.toString.call(thing).slice(8, -1);
+export const getType = (thing: any): Type =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (typeof Symbol !== "undefined" && thing[Symbol.toStringTag]) || (Object.prototype.toString.call(thing).slice(8, -1) as Type);
