198743238516a0dc7f7f2dc03869a6d792e91bc6
prisis <d.bannert@anolilab.de>
2024-02-28 12:32:13 +0100

    feat: adding template support
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/colorize/src/util/string-replace-all.ts
@@ -0,0 +1,36 @@
+/**
+ * Modified copy of https://github.com/webdiscus/ansis/blob/master/src/utils.js
+ *
+ * ISC License
+ *
+ * Copyright (c) 2023, webdiscus
+ */
+
+/**
+ * Replace all matched strings.
+ * Note: this implementation is over 30% faster than String.replaceAll().
+ */
+export const stringReplaceAll = (string_: string, searchValue: string, replaceValue: string): string => {
+    // visible style has empty open/close props
+    if (searchValue === "") {
+        return string_;
+    }
+
+    let pos = string_.indexOf(searchValue);
+    if (pos < 0) {
+        return string_;
+    }
+
+    const substringLength = searchValue.length;
+    let lastPos = 0;
+    let result = "";
+
+    // eslint-disable-next-line no-loops/no-loops,no-bitwise
+    while (~pos) {
+        result += string_.slice(lastPos, pos) + replaceValue;
+        lastPos = pos + substringLength;
+        pos = string_.indexOf(searchValue, lastPos);
+    }
+
+    return result + string_.slice(lastPos);
+};
