dd52da771beb5adf397a7252de70e5a377f45127
prisis <d.bannert@anolilab.de>
2024-03-09 15:56:28 +0100

    feat(fs): added isAccessibleSync and isAccessible
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/fs/src/constants.ts
@@ -0,0 +1,11 @@
+/** Is the path visible to the calling process? */
+export const F_OK = 0; // constants?.F_OK
+
+/** Is the path readable to the calling process? */
+export const R_OK = 4; // constants?.R_OK
+
+/** Is the path writable to the calling process? */
+export const W_OK = 2; // constants?.W_OK
+
+/** Is the path executable to the calling process? */
+export const X_OK = 1; // constants?.X_OK
