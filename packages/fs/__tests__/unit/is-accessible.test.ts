7d72bfdc547f9486af33908640282b617a15ab36
prisis <d.bannert@anolilab.de>
2024-03-09 18:28:15 +0100

    fix: fixed test for windows
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/fs/__tests__/unit/is-accessible.test.ts
+++ packages/fs/__tests__/unit/is-accessible.test.ts
@@ -13,6 +13,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, "..", "..", "tsconfig.json");
 const directory = __dirname;
 
+const isWindows = process.platform === "win32";
+
 describe.each([
     ["isAccessible", isAccessible],
     ["isAccessibleSync", isAccessibleSync],
@@ -22,24 +24,47 @@ describe.each([
         [file, F_OK, true, "file is accessible"],
         [file, R_OK, true, "file is readable"],
         [file, W_OK, true, "file is writable"],
-        [file, X_OK, false, "file is not executable"],
 
         [directory, undefined, true, "dir is accessible via default"],
         [directory, F_OK, true, "dir is accessible"],
         [directory, R_OK, true, "dir is readable"],
         [directory, W_OK, true, "dir is writable"],
-        [directory, X_OK, true, "dir is executable"],
 
         ["missing", undefined, false, "missing is not accessible via default"],
         ["missing", F_OK, false, "missing is not accessible"],
         ["missing", R_OK, false, "missing is not readable"],
         ["missing", W_OK, false, "missing is not writable"],
-        ["missing", X_OK, false, "missing is not executable"],
 
         [[file, directory, "missing"], undefined, [true, true, false], "accessible via default combination is as expected"],
         [[file, directory, "missing"], F_OK, [true, true, false], "accessible combination is as expected"],
         [[file, directory, "missing"], R_OK, [true, true, false], "readable combination is as expected"],
         [[file, directory, "missing"], W_OK, [true, true, false], "writable combination is as expected"],
+    ])(
+        "should should support different mods %s %s",
+        async (input: string[] | string, mode: number | undefined, expected: boolean[] | boolean, message: string) => {
+            expect.assertions(1);
+
+            // @ts-expect-error - ts cant figure out the type
+            let result = function_(input, mode);
+
+            // eslint-disable-next-line vitest/no-conditional-in-test
+            if (name === "isAccessible") {
+                // @ts-expect-error - we are testing the promise
+                result = await result;
+            }
+
+            expect(result, message).toStrictEqual(expected);
+        },
+    );
+
+    // on windows, the file is always executable
+    it.runIf(!isWindows).each([
+        [file, X_OK, false, "file is not executable"],
+
+        [directory, X_OK, true, "dir is executable"],
+
+        ["missing", X_OK, false, "missing is not executable"],
+
         [[file, directory, "missing"], X_OK, [false, true, false], "executable combination is as expected"],
     ])(
         "should should support different mods %s %s",
