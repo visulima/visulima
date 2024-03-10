280e787c1894317653359a5ac97dd439ccd925d8
Daniel Bannert <prisis@users.noreply.github.com>
2024-03-10 23:47:57 +0100

    refactor: moving apps into a new folder (#342)

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,14 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { mergeConfig } from "vite";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-    addons: [
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-a11y",
-        "@storybook/addon-actions/register",
-        "@storybook/addon-themes",
-    ],
+    addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-actions", "@storybook/addon-themes", "@storybook/addon-links", "@storybook/addon-docs"],
     docs: {
         autodocs: "tag",
     },
@@ -16,9 +13,10 @@ const config: StorybookConfig = {
         name: "@storybook/react-vite",
         options: {},
     },
+    staticDirs: ["../public"],
     stories: ["../packages/**/__stories__/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-    viteFinal: async (vConfig) => mergeConfig(vConfig, { plugins: [] }),
+    viteFinal: async (vConfig) => mergeConfig(vConfig, { plugins: [tsconfigPaths()] }),
 };
 
 // eslint-disable-next-line import/no-unused-modules
