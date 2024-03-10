236b69006d28591c60fcec5b51d196d69d7d92e2
prisis <d.bannert@anolilab.de>
2024-03-10 21:08:04 +0100

    feat: moving apps into a new folder
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,14 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths"
 
 const config: StorybookConfig = {
-    addons: [
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-a11y",
-        "@storybook/addon-actions/register",
-        "@storybook/addon-themes",
-    ],
+    addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-actions", "@storybook/addon-themes", "@storybook/addon-links", "@storybook/addon-docs"],
     docs: {
         autodocs: "tag",
     },
@@ -16,9 +11,10 @@ const config: StorybookConfig = {
         name: "@storybook/react-vite",
         options: {},
     },
+    staticDirs: ["../public"],
     stories: ["../packages/**/__stories__/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-    viteFinal: async (vConfig) => mergeConfig(vConfig, { plugins: [] }),
+    viteFinal: async (vConfig) => mergeConfig(vConfig, { plugins: [tsconfigPaths()] }),
 };
 
 // eslint-disable-next-line import/no-unused-modules
--- scripts/mv.cjs
+++
@@ -1,7 +0,0 @@
-
-const mvdir = require("mvdir");
-const path = require("node:path");
-
-const source = path.join(__dirname, "..", "storybook-static");
-
-mvdir(source, path.join(__dirname, "..", "storybook"), { copy: true });
