19059bfece4b80d7e9890101ab76dbeb61a8c32d
prisis <d.bannert@anolilab.de>
2024-03-23 09:17:13 +0100

    feat(fs): adding test
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/package/__tests__/unit/find-cache-dir.test.ts
+++ packages/package/__tests__/unit/find-cache-dir.test.ts
@@ -1,7 +1,38 @@
-import { describe } from "vitest";
+import { rm } from "node:fs/promises";
+
+import { temporaryDirectory } from "tempy";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+
 import { findCacheDirectory, findCacheDirectorySync } from "../../src";
+import { ensureDirSync, writeJsonSync } from "@visulima/fs";
+import { join } from "node:path";
 
 describe.each([
     ["findCacheDirectory", findCacheDirectory],
     ["findCacheDirectorySync", findCacheDirectorySync],
-])("%s", (name, function_) => {});
+])("%s", (name, function_) => {
+    let distribution: string;
+
+    beforeEach(async () => {
+        distribution = temporaryDirectory();
+    });
+
+    afterEach(async () => {
+        await rm(distribution, { recursive: true });
+    });
+
+    it("should return the cache directory path if it exists and is writeable", async () => {
+        expect.assertions(1);
+
+        ensureDirSync(join(distribution, "node_modules", ".cache", "test"));
+        writeJsonSync(join(distribution, "package.json"), {
+            name: "test"
+        })
+
+        const result = await findCacheDirectory("test", {
+            cwd: distribution,
+        });
+
+        expect(result).toEqual(expect.any(String));
+    });
+});
