c6fb0cfa02c88625fe5c785c7302cc346d36db39
Daniel Bannert <d.bannert@anolilab.de>
2024-03-24 21:29:31 +0100

    chore: fixed found issues on windows

--- packages/fs/__tests__/helpers.ts
+++ packages/fs/__tests__/helpers.ts
@@ -6,7 +6,7 @@ import { execSync } from "node:child_process";
 // eslint-disable-next-line import/prefer-default-export
 export const execScriptSync = (file: string, flags: string[] = [], environment: string[] = []): string => {
     const environmentVariables = environment.length > 0 ? `${environment.join(" ")} ` : "";
-    const cmd = `cross-env ${environmentVariables}node ${file} ${flags.join(" ")}`;
+    const cmd = `cross-env ${environmentVariables}node "${file}" ${flags.join(" ")}`;
     const result = execSync(cmd);
 
     // replace last newline in result
