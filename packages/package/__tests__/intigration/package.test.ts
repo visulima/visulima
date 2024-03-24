e6e5b358b56259c30517227d14749a1bec702968
prisis <d.bannert@anolilab.de>
2024-03-25 00:34:33 +0100

    fix: fixed test after pathe switch
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/package/__tests__/intigration/package.test.ts
+++ packages/package/__tests__/intigration/package.test.ts
@@ -1,6 +1,6 @@
-import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
+import { dirname, join } from "pathe";
 import { describe, expect, it } from "vitest";
 
 import { esc, execScriptSync } from "../helpers";
@@ -12,26 +12,26 @@ describe("usage `@visulima/package` npm package", () => {
     it(`should work as CommonJS package`, () => {
         expect.assertions(1);
 
-        const filename = join(__dirname, "../..", "__fixtures__/package/cjs/test.cjs");
+        const filename = join(__dirname, "..", "..", "__fixtures__/package/cjs/test.cjs");
 
         const received = execScriptSync(filename);
 
         expect(JSON.parse(esc(received))).toStrictEqual({
             name: "@visulima/package",
-            path: join(__dirname, "../..", "package.json"),
+            path: join(__dirname, "..", "..", "package.json"),
         });
     });
 
     it(`should work as ESM package`, async () => {
         expect.assertions(1);
 
-        const filename = join(__dirname, "../..", "__fixtures__/package/mjs/test.mjs");
+        const filename = join(__dirname, "..", "..", "__fixtures__/package/mjs/test.mjs");
 
         const received = execScriptSync(filename);
 
         expect(JSON.parse(esc(received))).toStrictEqual({
             name: "@visulima/package",
-            path: join(__dirname, "../..", "package.json"),
+            path: join(__dirname, "..", "..", "package.json"),
         });
     });
 });
