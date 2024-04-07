0ee589ddd976c92f4882e6f7e07086401f958776
prisis <d.bannert@anolilab.de>
2024-04-07 12:53:57 +0200

    fix(pail): fixed error handling as context
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- examples/pail/node/child-logger.js
+++
@@ -1,9 +0,0 @@
-import { pail } from "@visulima/pail";
-
-console.log("\n");
-
-const systemLogger = pail.scope("system");
-systemLogger.success("Hello from system logger");
-
-const childLogger = systemLogger.child("net");
-childLogger.success("Hello from child logger");
