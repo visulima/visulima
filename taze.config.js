cb8e48e45148a685b6d3a37eb99a452483ee5eb3
prisis <d.bannert@anolilab.de>
2024-04-11 20:31:17 +0200

    chore: replaced ncu with taze, update deps
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ taze.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "taze";
+
+export default defineConfig({
+    // ignore packages from bumping
+    exclude: [
+        "read-pkg-up",
+        "chalk",
+        "next",
+        "@types/node",
+        "prisma",
+        "@prisma/client",
+        "@types/swagger-ui-react",
+        "swagger-ui-dist",
+        "swagger-ui-react",
+        "eslint",
+        "eslint-plugin-vitest",
+    ],
+    // write to package.json
+    write: true,
+    ignorePaths: ["node_modules", "packages/**/dist", "packages/**/coverage", "packages/**/__fixtures__", "packages/**/__tests__"],
+    recursive: true,
+});
