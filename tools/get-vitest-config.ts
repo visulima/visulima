d4f326ca80bc4ef6fd86c84b3b99fb5d7b08a23a
prisis <d.bannert@anolilab.de>
2024-05-14 15:12:05 +0200

    build: fixed vitest
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- tools/get-vitest-config.ts
+++ tools/get-vitest-config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import type { UserConfig } from "vitest/config";
-import { defineConfig, configDefaults } from "vitest/config";
+import { defineConfig, configDefaults, coverageConfigDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export const getVitestConfig = (options: UserConfig = {}) => {
@@ -11,12 +11,13 @@ export const getVitestConfig = (options: UserConfig = {}) => {
     return defineConfig({
         ...options,
         test: {
+            ...configDefaults,
             coverage: {
-                all: true,
+                ...coverageConfigDefaults,
                 provider: "v8",
                 reporter: ["clover", "cobertura", "lcov", "text"],
                 include: ["src"],
-                exclude: [...configDefaults.coverage.exclude, "__fixtures__/**", "__bench__/**", "scripts/**"],
+                exclude: ["__fixtures__/**", "__bench__/**", "scripts/**"],
             },
             environment: "node",
             reporters: process.env.CI_PREFLIGHT ? ["basic", "github-actions"] : ["basic"],
@@ -27,7 +28,7 @@ export const getVitestConfig = (options: UserConfig = {}) => {
                 enabled: false,
             },
             ...options.test,
-            exclude: [...configDefaults.exclude, "__fixtures__/**", ...options.test?.exclude],
+            exclude: [...configDefaults.exclude, "__fixtures__/**", ...(options.test?.exclude ?? [])],
         },
     });
 };
