6e3d431611850ae1bab2305f1112d2ece6f239f3
Daniel Bannert <prisis@users.noreply.github.com>
2024-05-29 00:10:50 +0200

    fix(package): exposed missing implicitBaseUrlSymbol, readTsConfig, wr… (#416)

--- packages/package/__tests__/unit/read-tsconfig-absolute-path.test.ts
+++ packages/package/__tests__/unit/read-tsconfig-absolute-path.test.ts
@@ -11,7 +11,7 @@ import { join } from "@visulima/path";
 import { temporaryDirectory } from "tempy";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 
-import { readTsConfig } from "../../src/read-tsconfig";
+import { readTsConfig } from "../../src/tsconfig/read-tsconfig";
 import { getTscTsconfig } from "../helpers";
 
 describe("parse-tsconfig - absolute path", () => {
---
+++ packages/package/src/tsconfig/index.ts
@@ -0,0 +1,4 @@
+export type { TsConfigResult } from "./find-tsconfig";
+export { findTSConfig, findTsConfig, findTSConfigSync,findTsConfigSync } from "./find-tsconfig";
+export { implicitBaseUrlSymbol, readTsConfig } from "./read-tsconfig";
+export { writeTSConfig, writeTsConfig, writeTsConfigSync } from "./write-tsconfig";
