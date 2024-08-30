3965a4782ef59832b12832dd42962cf2fb4cc524
prisis <d.bannert@anolilab.de>
2024-08-30 23:36:06 +0200

    style(cerebro): cs fix
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/cerebro/__tests__/unit/cli.test.ts
+++ packages/cerebro/__tests__/unit/cli.test.ts
@@ -78,9 +78,11 @@ describe("cli", () => {
             options: [{ name: "requiredOption", required: true, type: String }],
         });
 
-        await expect(cli.run({
-            shouldExitProcess: false
-        })).rejects.toThrow('You called the command "test" without the required options: requiredOption');
+        await expect(
+            cli.run({
+                shouldExitProcess: false,
+            }),
+        ).rejects.toThrow('You called the command "test" without the required options: requiredOption');
     });
 
     it("should throw error when running a command with unknown options", async () => {
@@ -90,9 +92,11 @@ describe("cli", () => {
 
         cli.addCommand({ execute: vi.fn(), name: "test" });
 
-        await expect(cli.run({
-            shouldExitProcess: false
-        })).rejects.toThrow('Found unknown option "--unknownOption"');
+        await expect(
+            cli.run({
+                shouldExitProcess: false,
+            }),
+        ).rejects.toThrow('Found unknown option "--unknownOption"');
     });
 
     it("should throw error when running a command with conflicting options", async () => {
@@ -109,15 +113,17 @@ describe("cli", () => {
             ],
         });
 
-        await expect(cli.run({
-            shouldExitProcess: false
-        })).rejects.toThrow('You called the command "test" with conflicting options: option1 and option2');
+        await expect(
+            cli.run({
+                shouldExitProcess: false,
+            }),
+        ).rejects.toThrow('You called the command "test" with conflicting options: option1 and option2');
     });
 
     it("should not throw a error when running a command with one options that dont conflict", async () => {
         expect.assertions(1);
 
-        const cli = new Cli("MyCLI", { argv: ["test", "--option1", "value1",] });
+        const cli = new Cli("MyCLI", { argv: ["test", "--option1", "value1"] });
 
         const execute = vi.fn();
 
@@ -131,7 +137,7 @@ describe("cli", () => {
         });
 
         await cli.run({
-            shouldExitProcess: false
+            shouldExitProcess: false,
         });
 
         expect(execute).toHaveBeenCalledOnce();
