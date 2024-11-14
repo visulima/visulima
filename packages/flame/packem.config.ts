0df82f4c1dad6d551e1a02879bead006a40a198c
prisis <d.bannert@anolilab.de>
2024-11-15 00:12:38 +0100

    feat: more work
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ packages/flame/packem.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@visulima/packem/config";
+import transformer from "@visulima/packem/transformer/esbuild";
+import postcssLoader from "@visulima/packem/css/loader/postcss";
+import sourceMapLoader from "@visulima/packem/css/loader/sourcemap";
+import cssnanoMinifier from "@visulima/packem/css/minifier/cssnano";
+
+export default defineConfig({
+    transformer,
+    rollup: {
+        css: {
+            loaders: [postcssLoader, sourceMapLoader],
+            minifier: cssnanoMinifier,
+        },
+        url: {
+            exclude: []
+        }
+    },
+});
