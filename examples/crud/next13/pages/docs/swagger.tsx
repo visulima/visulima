349b19146a5aa118a5e4c1e31bc1c60c2f2e6128
prisis <d.bannert@anolilab.de>
2024-11-20 01:25:30 +0100

    chore: removed old next.js version examples
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- examples/crud/next13/pages/docs/swagger.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { getSwaggerStaticProps as getSwaggerStaticProperties, SwaggerPage } from "@visulima/api-platform/next";
-
-export const getStaticProps = getSwaggerStaticProperties(`${process.env.NEXT_PUBLIC_APP_ORIGIN}/api/swagger`);
-
-export default SwaggerPage(`${process.env.NEXT_PUBLIC_APP_NAME} swagger`);
