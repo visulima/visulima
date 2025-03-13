627b1ec6a8c3ee56d8451c03250c38e23365d64b
prisis <d.bannert@anolilab.de>
2025-03-13 21:52:28 +0100

    fix: correct zero-width character handling in string width calculations
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/string/src/constants.ts
+++ packages/string/src/constants.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import emojiRegex from "emoji-regex-xs";
+
 /**
  * ANSI escape characters
  */
@@ -31,14 +34,14 @@ export const END_CODE = 39;
 /**
  * Zero-width characters to remove, EXCLUDING zero-width joiner used in emoji
  */
-export const ZERO_WIDTH_REGEX = /[\u200B\uFEFF\u2060-\u2064]/g;
+export const RE_ZERO_WIDTH = /[\u200B\uFEFF\u2060-\u2064]/g;
 
 /**
  * RegExp pattern for ANSI escape sequences
  * Compiled once for better performance
  */
 // eslint-disable-next-line regexp/no-control-character,@rushstack/security/no-unsafe-regexp
-export const ESCAPE_PATTERN = new RegExp(`(?:\\${ANSI_CSI}(?<code>\\d+)m|\\${ANSI_ESCAPE_LINK}(?<uri>.*)${ANSI_ESCAPE_BELL})`);
+export const RE_ESCAPE_PATTERN = new RegExp(`(?:\\${ANSI_CSI}(?<code>\\d+)m|\\${ANSI_ESCAPE_LINK}(?<uri>.*)${ANSI_ESCAPE_BELL})`);
 
 /**
  * Map of ANSI style codes to their reset codes
@@ -82,3 +85,42 @@ export const RE_ONLY_WHITESPACE_WITH_AT_LEAST_ONE_NEWLINE = /^[ \t]*[\r\n][ \t\r
 
 // Precompile regular expressions to avoid recompilation during execution
 export const RE_MATCH_NEWLINES = /\r\n|\n|\r/g;
+
+/**
+ * Regular expression for ANSI escape sequences
+ * Used for parsing and handling ANSI color codes and formatting
+ */
+// eslint-disable-next-line no-control-regex,regexp/no-control-character,security/detect-unsafe-regex
+export const RE_ANSI = /[\u001B\u009B](?:[[()#;?]*(?:\d{1,4}(?:;\d{0,4})*)?[0-9A-ORZcf-n qry=><]|\]8;;.*?\u0007)/y;
+
+/**
+ * Regular expression for ANSI link end sequences
+ */
+// eslint-disable-next-line no-control-regex,regexp/no-control-character
+export const RE_ANSI_LINK_END = /\u001B\]8;;\u0007/y;
+
+/**
+ * Regular expression for control characters
+ */
+// eslint-disable-next-line no-control-regex,regexp/no-control-character,regexp/no-obscure-range
+export const RE_CONTROL = /[\u0000-\u0008\n-\u001F\u007F-\u009F]{1,1000}/y;
+
+/**
+ * Regular expression for emoji characters
+ */
+export const RE_EMOJI = emojiRegex();
+
+/**
+ * Regular expression for Latin characters
+ */
+export const RE_LATIN = /(?:[\u0020-\u007E\u00A0-\u00FF](?!\uFE0F)){1,1000}/y;
+
+/**
+ * Regular expression for Unicode modifiers
+ */
+export const RE_MODIFIER = /\p{M}+/gu;
+
+/**
+ * Regular expression for tab characters
+ */
+export const RE_TAB = /\t{1,1000}/y;
