bf93cd182e9881cee8e8026572fd265f2ad255fa
prisis <d.bannert@anolilab.de>
2025-06-01 22:34:43 +0200

    chore(flame): update PostCSS configuration and enhance Tailwind CSS integration
    - Added Tailwind CSS PostCSS plugin to the configuration for improved styling capabilities.
    - Introduced custom variants and additional Tailwind CSS plugins for typography and forms in the index.css file, enhancing the design flexibility.

--- packages/flame/postcss.config.cjs
+++ packages/flame/postcss.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
     plugins: {
+        "@tailwindcss/postcss": {},
         "postcss-focus-visible": {
             replaceWith: "[data-focus-visible-added]",
         },
