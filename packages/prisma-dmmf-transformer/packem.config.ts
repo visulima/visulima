c12a4828f7b934a2faa532c380210addefc13428
prisis <d.bannert@anolilab.de>
2025-09-10 20:16:42 +0200

    fix(prisma-dmmf-transformer): update package.json and pnpm-lock.yaml for compatibility
    - Restored require exports in package.json for CommonJS compatibility.
    - Added json-schema dependency version 0.4.0 in package.json.
    - Updated peer dependency version for vite in pnpm-lock.yaml to >=7.1.5.
    - Added new fixture entry in pnpm-lock.yaml for better organization.

--- packages/prisma-dmmf-transformer/packem.config.ts
+++ packages/prisma-dmmf-transformer/packem.config.ts
@@ -1,6 +1,7 @@
 import type { BuildConfig } from "@visulima/packem/config";
 import { defineConfig } from "@visulima/packem/config";
 import transformer from "@visulima/packem/transformer/esbuild";
+import isolatedDeclarationTransformer from "@visulima/packem/dts/isolated/transformer/typescript";
 
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
         },
     },
     transformer,
+    isolatedDeclarationTransformer,
     cjsInterop: true,
 }) as BuildConfig;
