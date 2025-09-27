e4e7ccd1f130389706cf8bff4f6769a3d00e447a
prisis <d.bannert@anolilab.de>
2025-09-28 01:59:58 +0200

    feat: added new http and fetch handler

--- examples/uploads/next13/pages/docs/swagger.tsx
+++
@@ -1,5 +0,0 @@
-import { getSwaggerStaticProps, SwaggerPage } from "@visulima/api-platform/next";
-
-export const getStaticProps = getSwaggerStaticProps(`${process.env.NEXT_PUBLIC_APP_ORIGIN}/api/swagger`);
-
-export default SwaggerPage(`${process.env.NEXT_PUBLIC_APP_NAME} swagger`);
