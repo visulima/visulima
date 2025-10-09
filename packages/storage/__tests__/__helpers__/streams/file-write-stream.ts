d435edec9a4fb31dd620ccf929e97ab4bb98e5d2
prisis <d.bannert@anolilab.de>
2025-10-09 17:10:02 +0200

    refactor(tests): enhance accessibility and consistency in stream classes
    - Updated access modifiers for properties and methods in FileWriteStream and RequestReadStream classes to public, improving clarity and consistency in test helpers.
    - Adjusted DiskStorage initialization in multipart tests to maintain consistent parameter ordering, enhancing readability and maintainability across test files.
    - Minor formatting improvements in streaming utility to ensure better code organization and readability.

--- packages/storage/__tests__/__helpers__/streams/file-write-stream.ts
+++ packages/storage/__tests__/__helpers__/streams/file-write-stream.ts
@@ -1,13 +1,13 @@
 import { PassThrough } from "node:stream";
 
 class FileWriteStream extends PassThrough {
-    get bytesWritten(): number {
+    public get bytesWritten(): number {
         // @ts-ignore
         return super.readableLength;
     }
 
     // eslint-disable-next-line class-methods-use-this
-    close(): void {}
+    public close(): void {}
 }
 
 export default FileWriteStream;
