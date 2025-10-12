1719cc3104d67cfaeb15b49fc9e4538ad320c283
prisis <d.bannert@anolilab.de>
2025-10-12 23:22:25 +0200

    fix: Adjusted the node engine requirement to support versions 20.19 and above.
    - Simplified the exports structure in package.json by consolidating types and default fields.
    - Updated devDependencies to their latest versions, including eslint, prettier, and typescript.
    - Adjusted the node engine requirement to support versions 20.19 and above.
    - Enhanced packem configuration to require CJS and include builtinNodeModules.
    
    BREAKING CHANGE: Adjusted the node engine requirement to support versions 20.19 and above.

--- packages/tsconfig/packem.config.ts
+++ packages/tsconfig/packem.config.ts
@@ -2,37 +2,26 @@ import typedocBuilder from "@visulima/packem/builder/typedoc";
 import type { BuildConfig } from "@visulima/packem/config";
 import { defineConfig } from "@visulima/packem/config";
 import transformer from "@visulima/packem/transformer/esbuild";
-import validate from "packages/jsdoc-open-api/src/validate";
 
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
-    node10Compatibility: {
-        writeToPackageJson: true,
-        typeScriptVersion: ">=5.0",
-    },
     rollup: {
         license: {
             path: "./LICENSE.md",
         },
+        requireCJS: {
+            builtinNodeModules: true
+        }
     },
     transformer,
     builder: {
         // TODO: add it back after its fixed in the alpha
         //typedoc: typedocBuilder,
     },
-    cjsInterop: true,
     typedoc: {
         format: "inline",
         readmePath: "./README.md",
         excludePrivate: true,
         excludeInternal: true,
-    },
-    // TODO: remove this after packem bug fix
-    validation: {
-        dependencies: {
-            unused: {
-                exclude: ["@visulima/fs", "@visulima/path", "jsonc-parser", "resolve-pkg-maps"],
-            },
-        },
     },
 }) as BuildConfig;
