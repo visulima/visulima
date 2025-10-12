6397e2e9a172a31d0983b0f34e9feabcc7bcf339
prisis <d.bannert@anolilab.de>
2025-10-12 23:41:08 +0200

    fix: allowed node 24
    - Deleted .eslintignore and .eslintrc.cjs files to eliminate outdated configurations.
    - Introduced eslint.config.js for a modern ESLint setup with improved ignore patterns.
    - Updated linting commands in package.json for simplicity and efficiency.
    - Adjusted package.json dependencies to their latest versions for better compatibility.

---
+++ packages/path/eslint.config.js
@@ -0,0 +1,22 @@
+import { createConfig } from "@anolilab/eslint-config";
+
+/** @type {import("@anolilab/eslint-config").PromiseFlatConfigComposer} */
+export default createConfig(
+    {
+        ignores: [
+            "dist",
+            "node_modules",
+            "coverage",
+            "__fixtures__",
+            "__docs__",
+            "examples",
+            "vitest.config.ts",
+            "packem.config.ts",
+            ".secretlintrc.cjs",
+            "package.json",
+            "playwright-setup.js",
+            ".prettierrc.cjs",
+            "README.md",
+        ],
+    },
+);
