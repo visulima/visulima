cea0533dda1161c2280a823d2ecaefb814818535
prisis <d.bannert@anolilab.de>
2025-10-12 23:33:36 +0200

    fix: update LICENSE.md and package.json dependencies
    - Enhanced LICENSE.md with clearer formatting and additional details for bundled dependencies and types.
    - Reordered dependencies in package.json for better organization and updated @pnpm/exe and @types/node to their latest versions.
    - Removed deprecated node10Compatibility settings from packem configuration.

--- packages/package/packem.config.ts
+++ packages/package/packem.config.ts
@@ -4,10 +4,6 @@ import transformer from "@visulima/packem/transformer/esbuild";
 
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
-    node10Compatibility: {
-        writeToPackageJson: true,
-        typeScriptVersion: ">=5.0",
-    },
     rollup: {
         license: {
             path: "./LICENSE.md",
