6e8791fdb4fedba86a5e80d94060264442694ee1
prisis <d.bannert@anolilab.de>
2025-11-23 21:00:29 +0100

    chore: clean up

--- packages/storage-client/examples/nextjs/.next/dev/static/chunks/63d81_next_dist_client_components_builtin_global-error_bfaf8a40.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/63d81_next_dist_a8ac0cea._.js"
-],
-    source: "dynamic"
-});
